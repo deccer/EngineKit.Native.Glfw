30f9a3a85b4e296655948c1384986473a929bc9a
deccer <decccer@outlook.com>
2022-09-16 00:05:04 +0200

    Add Example project

---
+++ examples/EngineKit.Native.Glfw.HelloWindow/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace EngineKit.Native.Glfw.HelloWindow;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        if (!Glfw.Init())
+        {
+            return;
+        }
+
+        var primaryMonitor = Glfw.GetPrimaryMonitor();
+        var primaryMonitorResolution = Glfw.GetVideoMode(primaryMonitor);
+
+        var screenWidth = primaryMonitorResolution.Width;
+        var screenHeight = primaryMonitorResolution.Height;
+
+        var windowWidth = (int)(screenWidth * 0.8f);
+        var windowHeight = (int)(screenHeight * 0.8f);
+
+        var windowHandle = Glfw.CreateWindow(
+            windowWidth,
+            windowHeight,
+            "HelloWindow",
+            IntPtr.Zero,
+            IntPtr.Zero);
+        if (windowHandle == IntPtr.Zero)
+        {
+            return;
+        }
+
+        Glfw.SetWindowPos(
+            windowHandle,
+            screenWidth / 2 - windowWidth / 2,
+            screenHeight / 2 - windowHeight / 2);
+
+        while (!Glfw.ShouldWindowClose(windowHandle))
+        {
+            Glfw.PollEvents();
+
+            Glfw.SwapBuffers(windowHandle);
+        }
+
+        Glfw.Terminate();
+    }
+}
